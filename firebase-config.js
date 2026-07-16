21a959be4684e11d9c6eb231afb216a7ab6540d2
Dennis Mutunga <heromtg7@gmail.com>
2026-07-16 13:57:44 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,8 +4,8 @@
 // enforced by your Firestore security rules (see README.md), not by hiding this file.
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +22,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
